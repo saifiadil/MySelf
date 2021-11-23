16b0cb759952a01ff65d05a2faffa4d295b73a0b
saifiadil <adilsaifi966@gmail.com>
2021-11-23 09:07:34 -0800

    myself

---
+++ first.js
@@ -0,0 +1,7 @@
+// const togglebutton =document.getElementsByClassName('toggle-button')[0]
+//         const navbarlink =document.getElementsByClassName('navbar-link')[0]
+//         togglebutton.addEventListener('click',()=>{
+//             navbarlink.classList.toggle('active')
+//         })
+
+        
